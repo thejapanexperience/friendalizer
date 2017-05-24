6222c0b359e1cecf098c851a3d8a949a56e8597c
Richard Mands <richard@eduku.org>
2017-05-24 15:53:43 +0800

    update

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-const {DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME} = process.env;
+// const {DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME} = process.env;
 
 const connection = mysql.createConnection({
   host     : 'sql9.freemysqlhosting.net',
